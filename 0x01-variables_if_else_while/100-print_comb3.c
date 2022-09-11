b62c28fbf56b05bbf93f6dfa17ca7007aa6b077f
Zukks01 <ajirsarah01@gmail.com>
2022-09-11 05:28:24 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,18 @@
  */
 int main(void)
 {
-	int i;
 	int c;
+	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		for (c = '0'; c <= '9'; c++)
+		for (i = '0'; i <= '9'; i++)
 		{
-			if (i < c)
+			if (c < i)
 			{
 				putchar(i);
 				putchar(c);
-				if (i != '8' || (i == '8' && c != '9'))
+				if (c != '8' || (c == '8' && i != '9'))
 				{
 					putchar(',');
 					putchar(' ');
