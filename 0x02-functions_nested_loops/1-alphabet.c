3791e45b756259d3637baa870b863b33507bc645
Zukks01 <ajirsarah01@gmail.com>
2022-09-13 12:48:26 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,10 +2,8 @@
 /**
  * main -  print_alphabet
  * prints the alphabet, in lowercase
- *
- * Return: 0
  */
-void print_alphabet(void)
+void print_alphabet:(void)
 {
 	char ch;
 
@@ -14,5 +12,4 @@ void print_alphabet(void)
 		_putchar(ch);
 	}
 	_putchar('\n');
-	return (0);
 }
