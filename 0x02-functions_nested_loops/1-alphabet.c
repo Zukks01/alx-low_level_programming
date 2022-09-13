3ddd07e81a88c93c983a4079cdedd8cf3804bf1b
Zukks01 <ajirsarah01@gmail.com>
2022-09-13 12:07:35 -0700

    hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,17 @@
 /**
  * main - a function that prints
  * the alphabet, in lowercase using _putchar
- * 
+ * Description: print_alphabet using the main function
  * Return: 0
  */
 void print_alphabet(void)
 {
 	char ch;
 
-	for (ch = 'a' ; ch <= 'z' ; ch++);
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
 	}
 	_putchar('\n');
 	return (0);
-
 }
