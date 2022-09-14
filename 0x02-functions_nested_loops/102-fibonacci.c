07f7626bf8760dcc191d5ed6d0730606fb0e04d7
Zukks01 <ajirsarah01@gmail.com>
2022-09-13 18:58:57 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the first 50
+ * Fibonacci numbers, starting with 1 and 2
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long count, i, j, k;
+
+	i = 0;
+	j = 1;
+	for (count = 0; count < 50; count++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu", k);
+		if (count == 49)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
