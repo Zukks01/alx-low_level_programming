f70ed58cc6d59894208995ccd7ff0538ac2555e6
Zukks01 <ajirsarah01@gmail.com>
2022-09-22 14:26:12 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,6 +9,7 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/**
+	 * main - Add one line to this code, so that the program prints a[2] = 98
 	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
