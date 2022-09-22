c183a0133b172cc8d01dc1df12714a9d3c41fb08
Zukks01 <ajirsarah01@gmail.com>
2022-09-22 07:25:16 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,8 @@ char *_strcat(char *dest, char *src)
 	int a = 0;
 	int b;
 
-	for (b = 0; dest[b] != '\0'; b++)
+	for (b = 0; dest[b] != '\0'; b++
+			)
 		while (src[a] != '\0')
 		{
 			dest[b + a] = src[a];
