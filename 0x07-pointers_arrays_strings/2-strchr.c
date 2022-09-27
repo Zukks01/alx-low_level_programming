2cfca7993decdd914937ef6fc32f5133ec6e1535
Zukks01 <ajirsarah01@gmail.com>
2022-09-27 06:01:49 -0700

    fixed

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,9 @@ char *_strchr(char *s, char c)
 
 	i = 0;
 	for (*(s + i) != '\0'; i++)
-	{
 		if (*(s + i) == c)
-		{
 			return (s + i);
-		}
 		if (*(s + i) == c)
-		{
 			return (s + i);
 			return ('\0');
-		}
-	}
 }
