4cad7678d1d7500eddb457ac07d6f3a31f7a96b8
Zukks01 <ajirsarah01@gmail.com>
2022-10-20 09:31:40 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,12 +7,12 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t n;
+	size_t i;
 
-	for (n = 0; h; n++)
+	for (i = 0; h; i++)
 	{
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 	}
-	return (n);
+	return (i);
 }
