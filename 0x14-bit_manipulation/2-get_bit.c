bcfa1d950528d1b8b039fcfb3f7e23f8b9a037a7
Zukks01 <ajirsarah01@gmail.com>
2022-10-28 04:22:53 -0700

    2-get_bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,7 +6,7 @@
  * Return: If an error occurs - -1.
  * Otherwise - The value of bit at index.
  */
-int get_bit(unsigned long int n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
