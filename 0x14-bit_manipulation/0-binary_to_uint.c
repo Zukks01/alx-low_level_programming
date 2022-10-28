6bfe66fb0e74a1948357d415b2225d92bc4d5ea4
Zukks01 <ajirsarah01@gmail.com>
2022-10-28 03:18:54 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,11 +20,13 @@ unsigned int binary_to_uint(const char *b)
 	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
-	}
+		}
 	if (b[len] & 1)
 	{
 		i += base_two;
+	}
 	}
 	return (i);
 }
